f601065e620972de5073471d12d561d7b8bd9fc2
Darius Thomas <dariusd.thomas@gmail.com>
2019-03-23 14:44:12 -0400

    'reactive keyboard to UPPER case and LOWER case

--- script.js
+++ script.js
@@ -21,23 +21,65 @@ $(document).ready(function () {
     $thirdRow.appendTo($keyboardDiv)
     $thirdRow.attr('class', 'd-flex flex-row justify-content-center');
 
+    let $fourthRow = $('<div></div>');
+    $fourthRow.attr('id', 'thirdRow');
+    $fourthRow.appendTo($keyboardDiv)
+    $fourthRow.attr('class', 'd-flex flex-row justify-content-center');
+
+    let $fourthRowBtn = $('<button></button>');
+    $fourthRowBtn.attr('type', 'button');
+    $fourthRowBtn.attr('class', 'btn btn-outline-secondary m-1')
+    $fourthRowBtn.css({
+        'height': '3em',
+        'width': '25em'
+    });
+    $fourthRowBtn.text(' ');
+    $fourthRowBtn.attr('id', 'a' + $fourthRowBtn.text().charCodeAt(0))
+    $fourthRowBtn.appendTo($fourthRow)
 
     $(document).ready(setKeyUnShifted)
+    
     $(document).keydown(function (e) {
-        if (e.keyCode == 16) {
+
+        if (e.shiftKey) {
             $firstRow.empty();
             $secondRow.empty();
             $thirdRow.empty();
             setKeyShifted()
         }
+        if (!e.shiftKey && $('#a' + (e.keyCode + 32)).length) {
+            console.log($('#a' + (e.keyCode + 32)).text())
+            $('#a' + (e.keyCode + 32)).css({
+                'background-color': 'gray',
+                'color': 'white'
+            })
+        } else if (e.shiftKey && $('#a' + e.keyCode).length) {
+            console.log($('#a' + e.keyCode).text())
+            $('#a' + e.keyCode).css({
+                'background-color': 'gray',
+                'color': 'white'
+            })
+        }
     });
 
     $(document).keyup(function (e) {
+
         if (e.keyCode == 16) {
             $firstRow.empty();
             $secondRow.empty();
             $thirdRow.empty();
             setKeyUnShifted()
+        }
+        if ($('#a' + e.keyCode).length) {
+            $('#a' + e.keyCode).css({
+                'background-color': 'white',
+                'color': 'gray'
+            })
+        } else if ($('#a' + (e.keyCode + 32)).length) {
+            $('#a' + (e.keyCode + 32)).css({
+                'background-color': 'white',
+                'color': 'gray'
+            })
         }
     });
 
@@ -52,11 +94,11 @@ $(document).ready(function () {
             $firstRowBtn.attr('type', 'button');
             $firstRowBtn.attr('class', 'btn btn-outline-secondary m-1')
             $firstRowBtn.css({
-                'height': '45px',
-                'width': '35px'
+                'height': '3em',
+                'width': '2.5em'
             })
             $firstRowBtn.text(firstRowKeys[i]);
-            $firstRowBtn.attr('id', firstRowKeys[i]);
+            $firstRowBtn.attr('id', 'a' + firstRowKeys[i].charCodeAt(0));
             $firstRowBtn.appendTo($firstRow)
         }
 
@@ -65,11 +107,11 @@ $(document).ready(function () {
             $secondRowBtn.attr('type', 'button');
             $secondRowBtn.attr('class', 'btn btn-outline-secondary m-1');
             $secondRowBtn.css({
-                'height': '45px',
-                'width': '35px'
+                'height': '3em',
+                'width': '2.5em'
             });
             $secondRowBtn.text(secondRowKeys[i]);
-            $secondRowBtn.attr('id', secondRowKeys[i]);
+            $secondRowBtn.attr('id', 'a' + secondRowKeys[i].charCodeAt(0));
             $secondRowBtn.appendTo($secondRow)
         }
 
@@ -78,29 +120,29 @@ $(document).ready(function () {
             $thirdRowBtn.attr('type', 'button');
             $thirdRowBtn.attr('class', 'btn btn-outline-secondary m-1')
             $thirdRowBtn.css({
-                'height': '45px',
-                'width': '35px'
+                'height': '3em',
+                'width': '2.5em'
             })
             $thirdRowBtn.text(thirdRowKeys[i]);
-            $thirdRowBtn.attr('id', thirdRowKeys[i]);
+            $thirdRowBtn.attr('id', 'a' + thirdRowKeys[i].charCodeAt(0));
             $thirdRowBtn.appendTo($thirdRow)
         }
     };
     function setKeyShifted() {
-        let firstRowKeys = ['Q','W','E','R','T','Y','U','I','O','P', '{', '}', '|']
-        let secondRowKeys = ['A','S','D','F','G','H','J','K','L', ':', '"'];
-        let thirdRowKeys = ['Z','X','C','V','B','N','M', '<', '>', '?'];
+        let firstRowKeys = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|']
+        let secondRowKeys = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"'];
+        let thirdRowKeys = ['Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?'];
 
         for (let i = 0; i < firstRowKeys.length; i++) {
             let $firstRowBtn = $('<button></button>');
             $firstRowBtn.attr('type', 'button');
             $firstRowBtn.attr('class', 'btn btn-outline-secondary m-1')
             $firstRowBtn.css({
-                'height': '45px',
-                'width': '35px'
+                'height': '3em',
+                'width': '2.5em'
             })
             $firstRowBtn.text(firstRowKeys[i]);
-            $firstRowBtn.attr('id', firstRowKeys[i]);
+            $firstRowBtn.attr('id', 'a' + firstRowKeys[i].charCodeAt(0));
             $firstRowBtn.appendTo($firstRow)
         }
 
@@ -109,11 +151,11 @@ $(document).ready(function () {
             $secondRowBtn.attr('type', 'button');
             $secondRowBtn.attr('class', 'btn btn-outline-secondary m-1');
             $secondRowBtn.css({
-                'height': '45px',
-                'width': '35px'
+                'height': '3em',
+                'width': '2.5em'
             });
             $secondRowBtn.text(secondRowKeys[i]);
-            $secondRowBtn.attr('id', secondRowKeys[i]);
+            $secondRowBtn.attr('id', 'a' + secondRowKeys[i].charCodeAt(0));
             $secondRowBtn.appendTo($secondRow)
         }
 
@@ -122,11 +164,11 @@ $(document).ready(function () {
             $thirdRowBtn.attr('type', 'button');
             $thirdRowBtn.attr('class', 'btn btn-outline-secondary m-1')
             $thirdRowBtn.css({
-                'height': '45px',
-                'width': '35px'
+                'height': '3em',
+                'width': '2.5em'
             })
             $thirdRowBtn.text(thirdRowKeys[i]);
-            $thirdRowBtn.attr('id', thirdRowKeys[i]);
+            $thirdRowBtn.attr('id', 'a' + thirdRowKeys[i].charCodeAt(0));
             $thirdRowBtn.appendTo($thirdRow)
         }
     };
